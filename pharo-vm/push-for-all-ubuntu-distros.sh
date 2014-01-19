2fdf330f8439e6b7f14c0c9fc6899db4b1611982
Damien Cassou <damien.cassou@gmail.com>
2014-01-19 19:23:24 +0100

    More Ubuntu distributions

--- pharo-vm/push-for-all-ubuntu-distros.sh
+++ pharo-vm/push-for-all-ubuntu-distros.sh
@@ -36,7 +36,7 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-declare -a distros=(saucy raring quantal precise lucid)
+declare -a distros=(trusty saucy raring quantal precise lucid)
 
 if [ $want_to_package_sources -eq 0 ]; then
     sources_option=''
